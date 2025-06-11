dc08a34f4e9ef057eaa938e3c2c760e87f69cd4b
jaimemartinsoliveira <1072224389@ulife.com.br>
2025-06-11 12:54:11 -0300

    Adicionado suporte a Markdown nos posts do blog com conversão automática para HTML no método `save`.

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,22 @@
+import markdown2
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=200, verbose_name="Título")
+
+    subtitle = models.CharField(max_length=200, verbose_name="Subtítulo")
+
     slug = models.SlugField(max_length=200, unique=True,
                             help_text="Uma versão curta e legível do título para URLs.")
-    content = models.TextField(verbose_name="Conteúdo")
+
+    content = models.TextField(verbose_name="Conteúdo (HTML ou Markdown)")
+
+    content_markdown = models.TextField(verbose_name="Conteúdo em Markdown")
+
+    content_html = models.TextField(editable=False)
+
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name='blog_posts', verbose_name="Autor")
     published_date = models.DateTimeField(default=timezone.now,
@@ -26,4 +36,10 @@ class Post(models.Model):
     def get_absolute_url(self):
         from django.urls import reverse
         return reverse('blog:post_detail', args=[self.slug])
+
+    def save(self, *args, **kwargs):
+        import markdown2
+        self.content_html = markdown2.markdown(self.content_markdown)
+        super().save(*args, **kwargs)
+
 
