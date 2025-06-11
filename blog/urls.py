f4ee1e024b54f7fd5416abc463f67da5973a5187
jaimemartinsoliveira <1072224389@ulife.com.br>
2025-06-11 10:00:19 -0300

    Corrigido comentário na configuração de rotas em `blog/urls.py`.

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.blog_list, name='list'), # ex: /blog/
+    path('', views.blog_list, name='list'),  # Rota com o nome 'list'
     path('post/<int:pk>/', views.blog_detail, name='detail'), # ex: /blog/post/5/
 ]
