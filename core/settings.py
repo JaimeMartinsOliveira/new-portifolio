8c366edd4387ced54ed3fb053331e67f773ce7e7
jaimemartinsoliveira <1072224389@ulife.com.br>
2025-06-12 11:46:44 -0300

    Adicionado `VisitorNotificationMiddleware` à lista de middlewares em `settings.py`.

--- core/settings.py
+++ core/settings.py
@@ -31,6 +31,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'captcha.middleware.VisitorNotificationMiddleware',
 ]
 
 # Configuração de Templates
