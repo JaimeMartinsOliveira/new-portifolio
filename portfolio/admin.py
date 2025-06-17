1e476e9fcd5a71781cf0931c832ceb1d51a55dfe
jaimemartinsoliveira <1072224389@ulife.com.br>
2025-06-16 22:36:59 -0300

    Registrado o modelo `VisitorCount` no admin com exibição de `id` e `count`.

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Experience, Skill, Formacao, SobreMim, Tecnologia, Projeto, Apresentacao
+from .models import Experience, Skill, Formacao, SobreMim, Tecnologia, Projeto, Apresentacao, VisitorCount
 
 admin.site.register(Experience)
 admin.site.register(Skill)
@@ -8,3 +8,7 @@ admin.site.register(SobreMim)
 admin.site.register(Tecnologia)
 admin.site.register(Projeto)
 admin.site.register(Apresentacao)
+
+@admin.register(VisitorCount)
+class VisitorCountAdmin(admin.ModelAdmin):
+    list_display = ('id', 'count')
