484e4a4cb314b0febccc1d62692cc9fe39a51778
jaimemartinsoliveira <1072224389@ulife.com.br>
2025-07-02 11:18:59 -0300

    Removidos comentários desnecessários em `entrypoint.sh`.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,25 +2,20 @@
 
 set -e
 
-# Cria as pastas necessárias, se não existirem
 mkdir -p /app/logs
 mkdir -p /app/staticfiles
 mkdir -p /app/media
 
-# Ajusta as permissões para o usuário 'app'
 echo "Ajustando permissões dos diretórios..."
 chown -R app:app /app/staticfiles
 chown -R app:app /app/media
 chown -R app:app /app/logs
 
-# Aplica as migrações do banco de dados
 echo "Aplicando as migrações do banco de dados..."
 python manage.py migrate --noinput
 
-# Coleta os arquivos estáticos
 echo "Coletando arquivos estáticos..."
 python manage.py collectstatic --noinput --clear
 
-# Inicia o servidor Gunicorn
 echo "Iniciando o servidor Gunicorn como usuário 'app'..."
 exec gosu app "$@"
